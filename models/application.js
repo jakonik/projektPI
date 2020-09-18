7463ed78fbdb8a83c3a310ba372e7a4bc8e56bc4
jakonik <jakala@protonmail.com>
2020-09-18 21:36:43 +0200

    usunięcie zbędnych plików

--- models/application.js
+++ models/application.js
@@ -35,44 +35,45 @@ module.exports.create = async (application) => {
     return new Promise((resolve, reject) => {
         const userModel = new User();
 
-        userModel.query('where', 'name', '=', application.usName, 'and', 'where', 'surname', '=', application.usSurname).fetch().then(function (model) {
-            console.log(model)
-            //tutaj wyświetlasz info "juz wypelniles ankiete"
-            reject('juz wypelniles ankiete');
-        }).catch((err) => {
-            new Promise(async (innerResolve, innerReject) => {
+        userModel.query('where', 'name', '=', application.usName, 'and', 'where', 'surname', '=', application.usSurname, 'and', 'where', 'mail', '=', application.usMail)
+            .fetch().then(function (model) {
+                console.log(model)
+                //tutaj wyświetlasz info "juz wypelniles ankiete"
+                reject('juz wypelniles ankiete');
+            }).catch((err) => {
+                new Promise(async (innerResolve, innerReject) => {
 
-                await new Application({
+                    await new Application({
 
-                    haslo: passHash,
-                    name: application.appName,
-                    mark: application.appMark,
-                    message: application.appMessage
-                }).save();
+                        haslo: passHash,
+                        name: application.appName,
+                        mark: application.appMark,
+                        message: application.appMessage
+                    }).save();
 
-                await innerResolve(new User({
+                    await innerResolve(new User({
 
-                    name: application.usName,
-                    surname: application.usSurname,
-                    mail: application.usMail,
-                    czy_ankieta_wyp: 1
-                }).save());
+                        name: application.usName,
+                        surname: application.usSurname,
+                        mail: application.usMail,
+                        czy_ankieta_wyp: 1
+                    }).save());
 
-                //wyświetlasz hash z toHash + hash
-                //     toSecretHash = toHash + passHash.passwordHash; 
-                //    const secretHash = sha512(toSecretHash, salt);
-                //     //console.log(secretHash);
-                //    return secretHash;
-                toSecretHash = toHash + application.appMark + application.appMessage + application.appName + passHash;
-                resolve(sha512(toSecretHash, application.appHaslo));
-            }).then((error) => {
-                //toSecretHash = toHash + passHash;
-                //resolve(sha512(toSecretHash, application.appHaslo));
-                // return error;
-                // console.log(error);
-            }).catch(console.log);
+                    //wyświetlasz hash z toHash + hash
+                    //     toSecretHash = toHash + passHash.passwordHash; 
+                    //    const secretHash = sha512(toSecretHash, salt);
+                    //     //console.log(secretHash);
+                    //    return secretHash;
+                    toSecretHash = toHash + application.appMark + application.appMessage + application.appName + passHash;
+                    resolve(sha512(toSecretHash, application.appHaslo));
+                }).then((error) => {
+                    //toSecretHash = toHash + passHash;
+                    //resolve(sha512(toSecretHash, application.appHaslo));
+                    // return error;
+                    // console.log(error);
+                }).catch(console.log);
 
-        })
+            })
 
 
 
